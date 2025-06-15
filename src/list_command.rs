afc89afa48c6efdec395b6f5e5c24a6881dbd90c
Felix Vadcard <vadcard.felix@gmail.com>
2025-06-15 10:12:56 +0200

    better hanling of result in list_command

--- src/list_command.rs
+++ src/list_command.rs
@@ -1,12 +1,16 @@
+use anyhow::Context;
+
 use crate::memory::memory;
 use crate::symlinks::get_symlink_path;
 
 pub fn list_versions() {
     let current_link = get_symlink_path();
-    let current = std::fs::read_link(&current_link).ok();
+    let current = std::fs::read_link(&current_link)
+        .with_context(|| "Cannot read current link")
+        .unwrap();
 
     for jdk in &memory().jdks {
-        let is_current = Some(jdk) == current.as_ref();
+        let is_current = jdk == &current;
         let marker = if is_current { "→" } else { " " };
         println!("{} {}", marker, jdk.display());
     }
